6815c4f6e08e27d40688489050e327eae07e15d2
etdube <etdube@gmail.com>
2023-03-02 17:40:45 -0500

    Contrôle du bras et limites selon angle (avec encodeur)

--- robot2023/src/main/java/frc/robot/RobotContainer.java
+++ robot2023/src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,16 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
 import frc.robot.commands.StopAll;
 import frc.robot.commands.StopArm;
 import frc.robot.commands.drivetrain.DriveArcade;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.GripperArm;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -23,124 +22,133 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-  private final CommandXboxController m_helperController =
-      new CommandXboxController(OperatorConstants.kHelperControllerPort);
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    private final CommandXboxController m_driverController =
+            new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    private final CommandXboxController m_helperController =
+            new CommandXboxController(OperatorConstants.kHelperControllerPort);
 
-  // The robot's subsystems and commands are defined here...
+    // The robot's subsystems and commands are defined here...
 
-  // Subsystems.
-  private final DriveTrain m_driveTrain = new DriveTrain();
-  private final GripperArm m_gripperArm = new GripperArm();
+    // Subsystems.
+    private final DriveTrain m_driveTrain = new DriveTrain();
+    private final GripperArm m_gripperArm = new GripperArm();
 
-  // Commands.
-  private final DriveArcade m_driveArcade = new DriveArcade(m_driverController.getHID(), m_driveTrain);
-  private final CommandBase m_shiftHigh = new InstantCommand(m_driveTrain::shiftHigh);
-  private final CommandBase m_shiftLow = new InstantCommand(m_driveTrain::shiftLow);
-  private final CommandBase m_stopAll = new StopAll(m_driveTrain);
-  private final StopArm m_stopArm = new StopArm(m_gripperArm);
+    // Commands.
+    private final DriveArcade m_driveArcade = new DriveArcade(m_driverController.getHID(), m_driveTrain);
+    private final CommandBase m_shiftHigh = new InstantCommand(m_driveTrain::shiftHigh);
+    private final CommandBase m_shiftLow = new InstantCommand(m_driveTrain::shiftLow);
+    private final CommandBase m_stopAll = new StopAll(m_driveTrain);
+    private final StopArm m_stopArm = new StopArm(m_gripperArm);
 
-  private final CommandBase m_raiseForeArm = new RunCommand(m_gripperArm::upForearm, m_gripperArm);
-  private final CommandBase m_lowerForeArm = new RunCommand(m_gripperArm::downForeArm, m_gripperArm);
-  private final CommandBase m_lockElbow = new InstantCommand(m_gripperArm::lockElbow);
-  private final CommandBase m_unlockElbow = new InstantCommand(m_gripperArm::unlockElbow);
+    private final CommandBase m_raiseForeArm = new RunCommand(m_gripperArm::upForearm, m_gripperArm);
+    private final CommandBase m_lowerForeArm = new RunCommand(m_gripperArm::downForeArm, m_gripperArm);
+    private final CommandBase m_lockElbow = new InstantCommand(m_gripperArm::lockElbow);
+    private final CommandBase m_unlockElbow = new InstantCommand(m_gripperArm::unlockElbow);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-    configureDefaultCommands();
-    configureAutonomousCommands();
-    configureSmartDashboard();
-  }
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        // Configure the trigger bindings
+        configureBindings();
+        configureDefaultCommands();
+        configureAutonomousCommands();
+        configureSmartDashboard();
+    }
 
-  private void configureDefaultCommands() {
-    m_driveTrain.setDefaultCommand(m_driveArcade);
-    m_gripperArm.setDefaultCommand(m_stopArm);
-  }
+    private void configureDefaultCommands() {
+        m_driveTrain.setDefaultCommand(m_driveArcade);
+        m_gripperArm.setDefaultCommand(m_stopArm);
+    }
 
-  private void configureAutonomousCommands() {
-    // m_autonomousDelayChooser.setDefaultOption("0", 0);
-    // for (int i = 1; i <= 15; ++i) {
-    //     m_autonomousDelayChooser.addOption(String.valueOf(i), i);
-    // }
+    private void configureAutonomousCommands() {
+        // m_autonomousDelayChooser.setDefaultOption("0", 0);
+        // for (int i = 1; i <= 15; ++i) {
+        //     m_autonomousDelayChooser.addOption(String.valueOf(i), i);
+        // }
 
-    // m_autonomousCommandChooser.setDefaultOption("None", m_autoNone);
-    // m_autonomousCommandChooser.addOption("Exit Tarmac (PID)", m_autoExitTarmacPID);
-    // //m_autonomousCommandChooser.addOption("Exit Tarmac (timer)", m_autoExitTarmacTimer);
-    // m_autonomousCommandChooser.addOption("Shoot 1 Ball", m_autoShoot1Ball);
-    // m_autonomousCommandChooser.addOption("Shoot 2 Balls 155deg", m_autoShoot2Balls155);
-    // m_autonomousCommandChooser.addOption("Shoot 2 Balls 180deg", m_autoShoot2Balls180);
+        // m_autonomousCommandChooser.setDefaultOption("None", m_autoNone);
+        // m_autonomousCommandChooser.addOption("Exit Tarmac (PID)", m_autoExitTarmacPID);
+        // //m_autonomousCommandChooser.addOption("Exit Tarmac (timer)", m_autoExitTarmacTimer);
+        // m_autonomousCommandChooser.addOption("Shoot 1 Ball", m_autoShoot1Ball);
+        // m_autonomousCommandChooser.addOption("Shoot 2 Balls 155deg", m_autoShoot2Balls155);
+        // m_autonomousCommandChooser.addOption("Shoot 2 Balls 180deg", m_autoShoot2Balls180);
 
-    // SmartDashboard.putData("Autonomous Delay", m_autonomousDelayChooser);
-    // SmartDashboard.putData("Autonomous Command", m_autonomousCommandChooser);
-}
+        // SmartDashboard.putData("Autonomous Delay", m_autonomousDelayChooser);
+        // SmartDashboard.putData("Autonomous Command", m_autonomousCommandChooser);
+    }
 
-private void configureSmartDashboard() {
-    SmartDashboard.putData(CommandScheduler.getInstance());
+    private void configureSmartDashboard() {
+        SmartDashboard.putData(CommandScheduler.getInstance());
 
-    SmartDashboard.putData(m_driveTrain);
-    // SmartDashboard.putData(m_intake);
-    // SmartDashboard.putData(m_shooter);
+        SmartDashboard.putData(m_driveTrain);
+        // SmartDashboard.putData(m_intake);
+        // SmartDashboard.putData(m_shooter);
 
-    SmartDashboard.putData("Commands/Shift High", m_shiftHigh);
-    SmartDashboard.putData("Commands/Shift Low", m_shiftLow);        
+        SmartDashboard.putData("Commands/Shift High", m_shiftHigh);
+        SmartDashboard.putData("Commands/Shift Low", m_shiftLow);
 
-    // SmartDashboard.putData("Commands/Move Shooter Up", m_moveShooterUp);
-    // SmartDashboard.putData("Commands/Move Shooter Down", m_moveShooterDown);
-    // SmartDashboard.putData("Commands/Shoot", m_shootNear);
-    // SmartDashboard.putData("Commands/Spin Shooter", m_spinShooter);
-    // SmartDashboard.putData("Commands/Stop Shooter", m_stopShooter);
+        // SmartDashboard.putData("Commands/Move Shooter Up", m_moveShooterUp);
+        // SmartDashboard.putData("Commands/Move Shooter Down", m_moveShooterDown);
+        // SmartDashboard.putData("Commands/Shoot", m_shootNear);
+        // SmartDashboard.putData("Commands/Spin Shooter", m_spinShooter);
+        // SmartDashboard.putData("Commands/Stop Shooter", m_stopShooter);
 
-    // SmartDashboard.putData("Commands/Latch Intake", m_latchIntake);
-    // SmartDashboard.putData("Commands/Unlatch Intake", m_unlatchIntake);
+        // SmartDashboard.putData("Commands/Latch Intake", m_latchIntake);
+        // SmartDashboard.putData("Commands/Unlatch Intake", m_unlatchIntake);
 
-    // SmartDashboard.putData("Commands/Unlatch Winch", m_unlatchWinch);
+        // SmartDashboard.putData("Commands/Unlatch Winch", m_unlatchWinch);
 
-    SmartDashboard.putData("Commands/Stop All", m_stopAll);
+        SmartDashboard.putData("Commands/Stop All", m_stopAll);
 
-    // SmartDashboard.putBoolean("Winch/Override 30 sec endmatch", false);
-}
+        // SmartDashboard.putBoolean("Winch/Override 30 sec endmatch", false);
+    }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    // new Trigger(m_exampleSubsystem::exampleCondition)
-    //     .onTrue(new ExampleCommand(m_exampleSubsystem));
+    /**
+     * Use this method to define your trigger->command mappings. Triggers can be created via the
+     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+     * predicate, or via the named factories in {@link
+     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+     * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+     * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+     * joysticks}.
+     */
+    private void configureBindings() {
+        // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+        // new Trigger(m_exampleSubsystem::exampleCondition)
+        //     .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-    //m_driverController.a().whileTrue(m_startMotor);
+        // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+        // cancelling on release.
+        //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+        //m_driverController.a().whileTrue(m_startMotor);
 
-    m_driverController.rightBumper().onTrue(m_shiftHigh);
-    m_driverController.leftBumper().onTrue(m_shiftLow);
-    m_driverController.b().whileTrue(m_lowerForeArm);
-    m_driverController.y().whileTrue(m_raiseForeArm);
-    m_driverController.x().onTrue(m_unlockElbow);
-    m_driverController.a().onTrue(m_lockElbow);
-    m_driverController.back().onTrue(new InstantCommand(m_gripperArm::toggleStage1));
-    m_driverController.start().onTrue(new InstantCommand(m_gripperArm::toggleStage2));
-  }
+        m_driverController.rightBumper().onTrue(m_shiftHigh);
+        m_driverController.leftBumper().onTrue(m_shiftLow);
+        m_driverController.b().whileTrue(m_lowerForeArm);
+        m_driverController.y().whileTrue(m_raiseForeArm);
+        m_driverController.x().onTrue(m_unlockElbow);
+        m_driverController.a().onTrue(m_lockElbow);
+        m_driverController.back().onTrue(new InstantCommand(m_gripperArm::toggleStage1));
+        m_driverController.start().onTrue(new InstantCommand(m_gripperArm::toggleStage2));
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    //return Autos.exampleAuto(m_exampleSubsystem);
-    return m_stopAll;
-  }
+        m_driverController.povLeft().onTrue(new InstantCommand(m_gripperArm::moveVerticalArmBackward));
+        m_driverController.povRight().onTrue(new InstantCommand(m_gripperArm::moveVerticalArmForward));
+    }
+
+    public void teleopInit() {
+      m_gripperArm.initTeleop();
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An example command will be run in autonomous
+        //return Autos.exampleAuto(m_exampleSubsystem);
+        return m_stopAll;
+    }
 }
--- robot2023/src/main/java/frc/robot/subsystems/GripperArm.java
+++ robot2023/src/main/java/frc/robot/subsystems/GripperArm.java
@@ -1,21 +1,21 @@
-  // Copyright (c) FIRST and other WPILib contributors.
-  // Open Source Software; you can modify and/or share it under the terms of
-  // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-  package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-  import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-  import edu.wpi.first.wpilibj.PneumaticsModuleType;
-  import edu.wpi.first.wpilibj.Solenoid;
-  import edu.wpi.first.wpilibj2.command.CommandBase;
-  import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static edu.wpi.first.wpilibj.PneumaticsModuleType.CTREPCM;
 
-  import static edu.wpi.first.wpilibj.PneumaticsModuleType.CTREPCM;
-
-  public class GripperArm extends SubsystemBase {
+public class GripperArm extends SubsystemBase {
     private final CANSparkMax m_foreArmLeader = new CANSparkMax(42, MotorType.kBrushed);
     private final CANSparkMax m_foreArmFollower = new CANSparkMax(43, MotorType.kBrushed);
 
@@ -24,16 +24,28 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
     private final Solenoid m_verticalArmStage2 = new Solenoid(CTREPCM, 2);
     private final Solenoid m_gripper = new Solenoid(CTREPCM, 6);
 
+    private final Encoder m_encoder = new Encoder(0, 1);
 
-    /** Creates a new GripperArm. */
+    private VerticalArmPosition currentVerticalArmPosition = VerticalArmPosition.REAR;
+
+    /**
+     * Creates a new GripperArm.
+     */
     public GripperArm() {
-      m_foreArmLeader.restoreFactoryDefaults();
-      m_foreArmFollower.restoreFactoryDefaults();
+        m_foreArmLeader.restoreFactoryDefaults();
+        m_foreArmFollower.restoreFactoryDefaults();
 
-      m_foreArmLeader.setIdleMode(IdleMode.kBrake);
-      m_foreArmFollower.setIdleMode(IdleMode.kBrake);
+        m_foreArmLeader.setIdleMode(IdleMode.kBrake);
+        m_foreArmFollower.setIdleMode(IdleMode.kBrake);
 
-      m_foreArmFollower.follow(m_foreArmLeader);
+        m_foreArmFollower.follow(m_foreArmLeader);
+
+        m_encoder.reset();
+    }
+
+    public void initTeleop() {
+        m_encoder.reset();
+        positionVerticalArm();
     }
 
     /**
@@ -42,12 +54,12 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
      * @return a command
      */
     public CommandBase exampleMethodCommand() {
-      // Inline construction of command goes here.
-      // Subsystem::RunOnce implicitly requires `this` subsystem.
-      return runOnce(
-          () -> {
-            /* one-time action goes here */
-          });
+        // Inline construction of command goes here.
+        // Subsystem::RunOnce implicitly requires `this` subsystem.
+        return runOnce(
+                () -> {
+                    /* one-time action goes here */
+                });
     }
 
     /**
@@ -56,61 +68,148 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
      * @return value of some boolean subsystem state, such as a digital sensor.
      */
     public boolean exampleCondition() {
-      // Query some boolean state, such as a digital sensor.
-      return false;
+        // Query some boolean state, such as a digital sensor.
+        return false;
     }
 
     @Override
     public void periodic() {
-      // This method will be called once per scheduler run
+        // This method will be called once per scheduler run
+        SmartDashboard.putNumber("Arm/Encoder", m_encoder.getDistance());
+        SmartDashboard.putString("Arm/VerticalPos", currentVerticalArmPosition.name());
     }
 
     @Override
     public void simulationPeriodic() {
-      // This method will be called once per scheduler run during simulation
+        // This method will be called once per scheduler run during simulation
     }
 
     public void upForearm() {
-      m_foreArmLeader.set(-0.25);
+        if (m_encoder.getDistance() >= currentVerticalArmPosition.maxAngleEncoderValue) {
+            m_foreArmLeader.set(0);
+        } else {
+            m_foreArmLeader.set(1.0);
+        }
     }
 
     public void downForeArm() {
-      m_foreArmLeader.set(0.25);
+        m_foreArmLeader.set(-1.0);
     }
 
     public void stopForearm() {
-      m_foreArmLeader.set(0.0);
+        m_foreArmLeader.set(0.0);
     }
 
     public void lockElbow() {
-      m_lockElbow.set(true);
+        m_lockElbow.set(true);
     }
 
     public void unlockElbow() {
-      m_lockElbow.set(false);
+        m_lockElbow.set(false);
     }
 
-    public void enableStage1() {
-      m_verticalArmStage1.set(true);
+    private void enableStage1() {
+        m_verticalArmStage1.set(true);
     }
 
-    public void disableStage1() {
-      m_verticalArmStage1.set(false);
+    private void disableStage1() {
+        m_verticalArmStage1.set(false);
     }
 
     public void toggleStage1() {
-      m_verticalArmStage1.toggle();
+        m_verticalArmStage1.toggle();
     }
 
-    public void enableStage2() {
-      m_verticalArmStage2.set(true);
+    private void enableStage2() {
+        m_verticalArmStage2.set(true);
     }
 
-    public void disableStage2() {
-      m_verticalArmStage2.set(false);
+    private void disableStage2() {
+        m_verticalArmStage2.set(false);
     }
 
     public void toggleStage2() {
-      m_verticalArmStage2.toggle();
+        m_verticalArmStage2.toggle();
     }
-  }
+
+    public void moveVerticalArmForward() {
+        currentVerticalArmPosition = currentVerticalArmPosition.moveForward(m_encoder.getDistance());
+        positionVerticalArm();
+    }
+
+    public void moveVerticalArmBackward() {
+        currentVerticalArmPosition = currentVerticalArmPosition.moveBackward(m_encoder.getDistance());
+        positionVerticalArm();
+    }
+
+    private void positionVerticalArm() {
+        switch (currentVerticalArmPosition) {
+            case REAR:
+                disableStage1();
+                disableStage2();
+                break;
+            case CENTRE:
+                enableStage1();
+                disableStage2();
+                break;
+            case FORWARD:
+                enableStage1();
+                enableStage2();
+                break;
+        }
+    }
+
+    public enum VerticalArmPosition {
+        REAR(22.5) {
+            @Override
+            public VerticalArmPosition moveForward(double angleEncoderValue) {
+                return CENTRE;
+            }
+
+            @Override
+            public VerticalArmPosition moveBackward(double angleEncoderValue) {
+                return REAR;
+            }
+        },
+        CENTRE(26) {
+            @Override
+            public VerticalArmPosition moveForward(double angleEncoderValue) {
+                return FORWARD;
+            }
+
+            @Override
+            public VerticalArmPosition moveBackward(double angleEncoderValue) {
+                // empèche de reculer le bras dans une position qui bloquerait
+                if (angleEncoderValue > REAR.maxAngleEncoderValue) {
+                    return this;
+                }
+                return REAR;
+            }
+        },
+        FORWARD(31.5) {
+            @Override
+            public VerticalArmPosition moveForward(double angleEncoderValue) {
+                return FORWARD;
+            }
+
+            @Override
+            public VerticalArmPosition moveBackward(double angleEncoderValue) {
+                // empèche de reculer le bras dans une position qui bloquerait
+                if (angleEncoderValue > CENTRE.maxAngleEncoderValue) {
+                    return this;
+                }
+                return CENTRE;
+            }
+        };
+
+        public final double maxAngleEncoderValue;
+
+        private VerticalArmPosition(double value) {
+            maxAngleEncoderValue = value;
+        }
+
+        public abstract VerticalArmPosition moveForward(double angleEncoderValue);
+
+        public abstract VerticalArmPosition moveBackward(double angleEncoderValue);
+    }
+}
